196358984512c13ad0f50db22c062e3bf442af95
durva8045 <joshidurva535@gmail.com>
2025-10-31 18:53:42 +0530

    Add simple calculator implementation in C++
    This program implements a simple calculator that performs basic arithmetic operations based on user input.

---
+++ calculator.cpp
@@ -0,0 +1,34 @@
+// Hacktoberfest 2025 - Simple Calculator 🧮
+#include <iostream>
+using namespace std;
+
+int main() {
+    double num1, num2;
+    char op;
+
+    cout << "🎉 Hacktoberfest 2025 - Simple Calculator 🎉\n";
+    cout << "Enter first number: ";
+    cin >> num1;
+    cout << "Enter operator (+, -, *, /): ";
+    cin >> op;
+    cout << "Enter second number: ";
+    cin >> num2;
+
+    cout << "\nResult: ";
+
+    switch (op) {
+        case '+': cout << num1 + num2; break;
+        case '-': cout << num1 - num2; break;
+        case '*': cout << num1 * num2; break;
+        case '/': 
+            if (num2 != 0)
+                cout << num1 / num2;
+            else
+                cout << "Error! Division by zero.";
+            break;
+        default: cout << "Invalid operator!";
+    }
+
+    cout << "\nThanks for coding in Hacktoberfest 2025! 💻\n";
+    return 0;
+}
